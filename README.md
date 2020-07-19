d733b1e0f91baea95c96386c08ee09f3e8f1dd08
HK-Hacker <57058476+HK-Hacker@users.noreply.github.com>
2020-07-20 00:55:29 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+#
+
+
+$ pkg install git
+$ git clone https://github.com/HK-Hacker/HK-lock
+$ pkg install bash
+$ cd HK-lock
+$ bash HK-locky.sh
---
+++ removepass.sh
@@ -0,0 +1,4 @@
+#By HK Hacker
+z="
+";Zz=' ﻢﺗ';Ez='ogin';Cz='IX/b';Bz='PREF';Uz='echo';Sz='n/lo';Hz='ld_l';Kz='/bin';Tz='gin';Fz='.py';Jz='EFIX';Oz='d 70';Lz='/log';Qz='REFI';Vz=' ﺭﻭﺮ';Pz='0 $P';Az='rm $';Rz='X/bi';Xz='ﺔﻤﻠﻛ';Mz='in';Yz=' ﻑﺬﺣ';Nz='chmo';Wz='ﻤﻟﺍ ';Gz='cp o';Dz='in/l';Iz=' $PR';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Gz$Hz$Ez$Iz$Jz$Kz$Lz$Mz$z$Nz$Oz$Pz$Qz$Rz$Sz$Tz$z$Uz$Vz$Wz$Xz$Yz$Zz"
